8506c99fe55240b8161529c6fd21e2a05cb9d327
d-beech <datubeechmusic@gmail.com>
2024-10-29 10:40:03 +1300

    Fixed superuser scripts

--- myproject/management/commands/createsu.py
+++ myproject/management/commands/createsu.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import User
 class Command(BaseCommand):
  
     def handle(self, *args, **options):
-        if not User.objects.filter(username="todoadmin").exists():
+        if not User.objects.filter(username="datub").exists():
             User.objects.create_superuser("datub", "datu@gmail.com", "password")
